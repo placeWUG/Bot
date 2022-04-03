6f2815ff829c037f325ee8d6e1ee1b873823ae6e
Hannes Güdelhöfer <hannes@guedelhoefer.de>
2022-04-03 21:17:50 +0200

    version

--- placedebot.user.js
+++ placedebot.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PlaceDE Bot
 // @namespace    https://github.com/PlaceDE/Bot
-// @version      15
+// @version      16
 // @description  /r/place bot
 // @author       NoahvdAa, reckter, SgtChrome, nama17
 // @match        https://www.reddit.com/r/place/*
@@ -21,7 +21,7 @@ var placeOrders = [];
 var accessToken;
 var canvas = document.createElement('canvas');
 
-const VERSION = 15
+const VERSION = 16
 var UPDATE_PENDING = false;
 
 const COLOR_MAPPINGS = {
